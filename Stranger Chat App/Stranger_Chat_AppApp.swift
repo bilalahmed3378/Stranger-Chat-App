df284f817eda48405697f65e27a8b86b7df3e233
bilalahmed3378 <109059774+bilalahmed3378@users.noreply.github.com>
2023-02-16 09:53:21 +0500

    Initial Commit

---
+++ Stranger Chat App/Stranger_Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stranger_Chat_AppApp.swift
+//  Stranger Chat App
+//
+//  Created by Bilal Ahmed on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Stranger_Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
